0dfe8cbbd47abcdead5d3676d63d1e350ee48ef2
CarlosEsteban19 <153372684+CarlosEsteban19@users.noreply.github.com>
2024-07-17 14:01:12 -0400

    Update __init__.py

--- src/persistence/__init__.py
+++ src/persistence/__init__.py
@@ -8,21 +8,25 @@ from utils.constants import REPOSITORY_ENV_VAR
 
 repo: Repository
 
-if os.getenv(key=REPOSITORY_ENV_VAR) == "db":
-    from src.persistence.db import DBRepository
+if os.getenv(key=REPOSITORY_ENV_VAR) == "memory":
+    from src.persistence.memory import MemoryRepository
+    
+    repo = MemoryRepository()
+    
 
-    repo = DBRepository()
 elif os.getenv(REPOSITORY_ENV_VAR) == "file":
     from src.persistence.file import FileRepository
 
     repo = FileRepository()
+
 elif os.getenv(REPOSITORY_ENV_VAR) == "pickle":
     from src.persistence.pickled import PickleRepository
 
     repo = PickleRepository()
-else:
-    from src.persistence.memory import MemoryRepository
 
-    repo = MemoryRepository()
+else:
+    from src.persistence.db import DBRepository
+
+    repo = DBRepository()
 
 print(f"Using {repo.__class__.__name__} as repository")
